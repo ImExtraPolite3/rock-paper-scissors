ad50722dda61d9aabd26c0563c4967927df75fcd
ExtraPolite <imextrapolite2@gmail.com>
2024-04-23 17:52:56 -0400

    get the game working

--- game.js
+++ game.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let computer = Math.floor((Math.random() * 3) + 1);
 
@@ -19,10 +16,46 @@ function getHumanChoice() {
   return human;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(); 
-
 function playRound(humanChoice, ComputerChoice) {
-  if(humanSelection == 'rock' && computerSelection == 'rock') {
+  if (humanChoice == 'rock' && ComputerChoice == 'rock') {
+    return 'tie';
+  } else if (humanChoice == 'rock' && ComputerChoice == 'paper') {
+    return 'you lose';
+  } else if (humanChoice == 'rock' && ComputerChoice == 'scissors') {
+    return 'you win';
+  } else if (humanChoice == 'paper' && ComputerChoice == 'rock') {
+    return 'you win';
+  } else if (humanChoice == 'paper' && ComputerChoice == 'paper') {
+    return 'tie';
+  } else if (humanChoice == 'paper' && ComputerChoice == 'scissors') {
+    return 'you lose';
+  } else if (humanChoice == 'scissors' && ComputerChoice == 'rock') {
+    return 'you lose';
+  } else if (humanChoice == 'scissors' && ComputerChoice == 'paper') {
+    return 'you win';
+  } else if (humanChoice == 'scissors' && ComputerChoice == 'scissors') {
+    return 'tie';
   }
 }
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice(); 
+    console.log(playRound(humanSelection, computerSelection));
+
+    if (playRound(humanSelection, computerSelection) == 'you win')  {
+      humanScore++;
+    } else if (playRound(humanSelection, computerSelection) == 'you lose') {
+      computerScore++;
+    }
+  }
+
+  console.log('human: ' + humanScore);
+  console.log('computer: ' + computerScore);
+}
+
+playGame();
