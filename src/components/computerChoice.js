ad3a333ce0039fa3d91529409d69875988bcaa40
ExtraPolite <imextrapolite2@gmail.com>
2024-11-14 16:54:05 -0500

    change name of file and content within file

--- src/components/computerChoice.js
+++ src/components/computerChoice.js
@@ -1,11 +1,5 @@
-function computerChoice() {
+export default function computerChoice() {
   const getNum = Math.floor(Math.random() * 3) + 1;
 
   return getNum === 1 ? 'rock' : getNum === 2 ? 'paper' : 'scissor';
 }
-
-function userChoice(clicked) {
-  return clicked;
-}
-
-export { computerChoice, userChoice };
