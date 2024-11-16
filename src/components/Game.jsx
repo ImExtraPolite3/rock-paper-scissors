6b45bb005504ab81c11505dcdca78247869e7ee1
ExtraPolite <imextrapolite2@gmail.com>
2024-11-16 14:23:25 -0500

    add score

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,14 +12,21 @@ function ButtonProp({ name, whenClick }) {
   );
 }
 
-function CreateButton({ setUser }) {
+function CreateButton({ setUser, setWins, setLoses }) {
   return buttonChoices.map((eachButton) => {
     return (
       <>
         <ButtonProp
           name={eachButton}
           whenClick={(e) => {
-            setUser(gameLogic(e.target.textContent, computerChoice()));
+            let playGame = gameLogic(e.target.textContent, computerChoice());
+            setUser(playGame);
+
+            if (playGame === 'player wins') {
+              setWins((prevWins) => prevWins + 1);
+            } else if (playGame === 'computer wins') {
+              setLoses((prevLose) => prevLose + 1);
+            }
           }}
         />
       </>
@@ -28,12 +35,18 @@ function CreateButton({ setUser }) {
 }
 
 export default function DisplayButton() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState('Click button to start game');
+  const [wins, setWins] = useState(0);
+  const [loses, setLoses] = useState(0);
 
   return (
     <div>
-      <p>{user}</p>
-      <CreateButton setUser={setUser} />
+      <div className="scores">
+        <h3 className="wins">{wins}</h3>
+        <h3 className="loses">{loses}</h3>
+      </div>
+      <h1>{user}</h1>
+      <CreateButton setUser={setUser} setWins={setWins} setLoses={setLoses} />
     </div>
   );
 }
