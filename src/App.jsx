6949c4be7221e7dc8728000ae27577761860fe3d
ExtraPolite <imextrapolite2@gmail.com>
2024-11-18 01:44:00 -0500

    change where Game.jsx will be rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import DisplayButton from './components/Game';
 import StartPage from './components/StartPage';
 
 function App() {
   return (
     <>
       <StartPage />
-      <DisplayButton />
     </>
   );
 }
--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import DisplayButton from './Game';
+
 export default function StartPage() {
+  const [start, setStart] = useState();
+  const [hide, setHide] = useState('');
+
+  const handleSetStart = () => {
+    setStart(<DisplayButton />);
+    setHide('none');
+  };
+
   return (
-    <div className="open-page">
-      <h1 className="game-title">ROCK PAPER SCISSORS</h1>
-      <button className="start-game">
-        <img src="startbutton.svg" alt="start-button" width="50px" />
-      </button>
-    </div>
+    <>
+      <div className="open-page" style={{ display: hide }}>
+        <h1 className="game-title">ROCK PAPER SCISSORS</h1>
+        <button className="start-game" onClick={handleSetStart}>
+          <img src="startbutton.svg" alt="start-button" width="50px" />
+        </button>
+      </div>
+      <div className="game">{start}</div>
+    </>
   );
 }
