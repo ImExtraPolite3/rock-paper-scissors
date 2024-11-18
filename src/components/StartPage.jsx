f0a6e7bf3706895e54274cdc2c744b52781ed8b6
ExtraPolite <imextrapolite2@gmail.com>
2024-11-17 19:52:06 -0500

    add start page components add img for start button add styling

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,3 +1,10 @@
 export default function StartPage() {
-  return <div className="start-page"></div>;
+  return (
+    <div className="open-page">
+      <h1 className="game-title">ROCK PAPER SCISSORS</h1>
+      <button className="start-game">
+        <img src="startbutton.svg" alt="start-button" width="50px" />
+      </button>
+    </div>
+  );
 }
