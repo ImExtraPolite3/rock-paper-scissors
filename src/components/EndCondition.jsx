13d3a82bf9b31d590f8a0cc730817f60634cf85d
ExtraPolite <imextrapolite2@gmail.com>
2024-11-23 00:00:39 -0500

    finish styling. Need to fix game logic

--- src/components/EndCondition.jsx
+++ src/components/EndCondition.jsx
@@ -3,14 +3,16 @@ import { useState } from 'react';
 export default function EndCondition({ condition, reset, modal }) {
   return (
     <div className="end-condition" style={{ display: modal }}>
-      <h1>{condition}</h1>
-      <button
-        onClick={() => {
-          reset();
-        }}
-      >
-        Reset Game
-      </button>
+      <div>
+        <h1>{condition}</h1>
+        <button
+          onClick={() => {
+            reset();
+          }}
+        >
+          Reset Game
+        </button>
+      </div>
     </div>
   );
 }
