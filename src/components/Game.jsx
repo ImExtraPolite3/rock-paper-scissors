8c27708ecd904f06cb38f16057b872640a6c0caa
ExtraPolite <imextrapolite2@gmail.com>
2024-11-23 21:35:53 -0500

    add buttons

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,37 @@
+const buttonName = ['rock', 'paper', 'scissors'];
+const imgs = ['rock.svg', 'paper.svg', 'scissors.svg'];
+
+function ButtonProp({ name, img, whenClick }) {
+  return (
+    <>
+      <button className={name} onClick={whenClick}>
+        <img src={img} alt={name} />
+        <p>{name}</p>
+      </button>
+    </>
+  );
+}
+
+function CreateButton() {
+  return buttonName.map((eachButton, index) => {
+    return (
+      <ButtonProp
+        key={index}
+        name={eachButton}
+        img={imgs[index]}
+        whenClick={() => {
+          console.log(eachButton);
+        }}
+      />
+    );
+  });
+}
+
 export default function Game() {
   return (
     <>
       <h1>Hello</h1>
+      <CreateButton />
     </>
   );
 }
